6b65cdb1dc5d2cdf65912c98817ba6f16b07a396
hartnetl <laura.hartnett@outlook.com>
2021-09-23 16:09:17 +0000

    Set up env and init files

---
+++ taskmanager/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+# import env to use our hidden environment variables
+if os.path.exists("env.py"):
+    import env  # noqa <- this removes linting warning
+
+# Create an instance of the imported Flask class
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Create an instand of the imported alchemy class
+db = SQLAlchemy(app)
+
+from taskmanager import routes  # noqa
