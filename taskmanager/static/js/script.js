c74db0e327e945f66ed0cbffbb7040806e9a5b12
hartnetl <laura.hartnett@outlook.com>
2021-09-23 17:24:03 +0000

    Add navabr

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav inialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
+
