13d9950569796f844e59ce8e97bd2a73f83a9c21
hartnetl <laura.hartnett@outlook.com>
2021-09-23 18:09:08 +0000

    Add post functionality for users to create new category

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -15,4 +15,14 @@ def categories():
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    # This sets the post method to allow users to add category to the database
+    if request.method == "POST":
+        # Create new category variable
+        # copy the variable from the model to make sure it's right (category_name)
+        # Set it to the value of the input from the form
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        # When someone adds a category, redirect them to the updated categories page
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
