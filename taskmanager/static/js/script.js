3a2db5cd5425cc0ce306ac5ccf17d6036b40338b
hartnetl <laura.hartnett@outlook.com>
2021-09-24 11:53:45 +0000

    Add confirmation modal to delete

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidenav inialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(elems, options);
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // modal initialization
+    let delModal = document.querySelectorAll('.modal');
+    M.Modal.init(delModal);
   });
 
