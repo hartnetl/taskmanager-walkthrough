f59c937fc6a207be61150e537a1ee4b7f84b0340
hartnetl <laura.hartnett@outlook.com>
2021-09-24 17:29:14 +0000

    Update herokus DATABASE_URL

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # import env to use our hidden environment variables
@@ -12,7 +13,10 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == TRUE:
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
 
 # Create an instand of the imported alchemy class
 db = SQLAlchemy(app)
