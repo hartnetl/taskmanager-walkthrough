b10978ffcb5596a330e7b021fd52b6dc2639722d
hartnetl <laura.hartnett@outlook.com>
2021-09-24 14:41:27 +0000

    Get task cards to view the same way as catgegories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.is_urgent).all())
+    return render_template("tasks.html", tasks=tasks)
 
 ##### CATEGORIES #####
 
